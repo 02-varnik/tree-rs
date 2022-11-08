65fb77f6398867886800a130dcf5f1785230fcfd
Parthtrap <mttparth306@gmail.com>
2022-11-09 02:01:35 +0530

    Sign Up Page Layout Fixed

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,8 +4,8 @@ import Signup from "../components/Signup";
 export default function SignupPage() {
   return (
     <>
-      <div className="min-h-full mt-16 h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
+      <div className="flex justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
           <Header
             heading="Signup to create an account"
             paragraph="Already have an account? "
